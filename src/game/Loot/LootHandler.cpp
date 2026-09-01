bc399e5a1b9db2da650e72e90d2bb713dcbc4282
Tim <98615444+T-imothy@users.noreply.github.com>
2026-09-01 16:11:08 -0500

    Fix group loot method validation

--- src/game/Loot/LootHandler.cpp
+++ src/game/Loot/LootHandler.cpp
@@ -265,7 +265,7 @@ void WorldSession::HandleLootMethodOpcode(WorldPacket& recv_data)
         return;
     /********************/
 
-    if (lootMethod > MASTER_LOOT)
+    if (lootMethod > NEED_BEFORE_GREED)
         return;
 
     if (lootMethod == MASTER_LOOT)
